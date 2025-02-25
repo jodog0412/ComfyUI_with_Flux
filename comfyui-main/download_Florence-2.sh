82ebae94cb355104bc5cc5f33a027fe1fbeb58e3
Hyunsung <jodog0412@gmail.com>
2025-02-26 02:22:05 +0900

    🐛 fix: fix bug

--- comfyui-main/download_Florence-2.sh
+++ comfyui-main/download_Florence-2.sh
@@ -2,22 +2,14 @@
 
 echo "Downloading Florence 2 models"
 mkdir -p /ComfyUI/models/LLM/
-# cd /ComfyUI/models/LLM/
+cd /ComfyUI/models/LLM/
 
 file="Florence-2-large-ft.bin"
-# url="https://huggingface.co/microsoft/Florence-2-large-ft/resolve/main/pytorch_model.bin?download=true"
-
-# if [ -f "$file" ]; then
-#     echo "$file already exists."
-# else
-#     echo "Downloading $file"
-#     wget -O $file $url --progress=bar:force:noscroll
-# fi
+url="https://huggingface.co/microsoft/Florence-2-large-ft/resolve/main/pytorch_model.bin?download=true"
 
 if [ -f "$file" ]; then
     echo "$file already exists."
 else
     echo "Downloading $file"
-    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download microsoft/Florence-2-large-ft --include '*.bin' --local-dir /ComfyUI/models/LLM
-    mv /ComfyUI/models/LLM/pytorch_model.bin /ComfyUI/models/LLM/$file
+    wget -O $file $url --progress=bar:force:noscroll
 fi
--- comfyui-main/download_Janus-Pro.sh
+++ comfyui-main/download_Janus-Pro.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "Downloading Florence 2 models"
+echo "Downloading Janus-Pro-1B model"
 mkdir -p /ComfyUI/models/Janus-Pro/Janus-Pro-1B
+cd /ComfyUI/models/Janus-Pro/Janus-Pro-1B
 
 file="Janus-Pro-1B.bin"
+url="https://huggingface.co/deepseek-ai/Janus-Pro-1B/resolve/main/pytorch_model.bin?download=true"
 
 if [ -f "$file" ]; then
     echo "$file already exists."
 else
     echo "Downloading $file"
-    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download deepseek-ai/Janus-Pro-1B --local-dir /ComfyUI/models/Janus-Pro/Janus-Pro-1B
-    mv /ComfyUI/models/Janus-Pro/Janus-Pro-1B/pytorch_model.bin /ComfyUI/models/Janus-Pro/Janus-Pro-1B/Janus-Pro-1B.bin
+    wget -O $file $url --progress=bar:force:noscroll
 fi
