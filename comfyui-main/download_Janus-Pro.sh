128fcb1910f70de4b37d8872a068744921346972
Hyunsung <jodog0412@gmail.com>
2025-02-26 07:27:39 +0900

    ✅ update: update jdg-comfyui 1.2.0

--- comfyui-main/download_Janus-Pro.sh
+++ comfyui-main/download_Janus-Pro.sh
@@ -2,14 +2,13 @@
 
 echo "Downloading Janus-Pro-1B model"
 mkdir -p /ComfyUI/models/Janus-Pro/Janus-Pro-1B
-cd /ComfyUI/models/Janus-Pro/Janus-Pro-1B
 
 file="Janus-Pro-1B.bin"
-url="https://huggingface.co/deepseek-ai/Janus-Pro-1B/resolve/main/pytorch_model.bin?download=true"
 
 if [ -f "$file" ]; then
     echo "$file already exists."
 else
     echo "Downloading $file"
     wget -O $file $url --progress=bar:force:noscroll
+    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download deepseek-ai/Janus-Pro-1B --include "*.bin" --local-dir /ComfyUI/models/Janus-Pro/Janus-Pro-1B
 fi
