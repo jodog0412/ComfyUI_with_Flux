00e65effc0960cea2815ff2bab1a7e4bcb883f87
Hyunsung <jodog0412@gmail.com>
2025-02-26 06:15:02 +0900

    ✅ update: update jdg-comfyui 1.2.0

--- comfyui-main/download_Florence-2.sh
+++ comfyui-main/download_Florence-2.sh
@@ -2,14 +2,21 @@
 
 echo "Downloading Florence 2 models"
 mkdir -p /ComfyUI/models/LLM/
-cd /ComfyUI/models/LLM/
+# cd /ComfyUI/models/LLM/
 
 file="Florence-2-large-ft.bin"
-url="https://huggingface.co/microsoft/Florence-2-large-ft/resolve/main/pytorch_model.bin?download=true"
+# url="https://huggingface.co/microsoft/Florence-2-large-ft/resolve/main/pytorch_model.bin?download=true"
+
+# if [ -f "$file" ]; then
+#     echo "$file already exists."
+# else
+#     echo "Downloading $file"
+#     wget -O $file $url --progress=bar:force:noscroll
+# fi
 
 if [ -f "$file" ]; then
     echo "$file already exists."
 else
     echo "Downloading $file"
-    wget -O $file $url --progress=bar:force:noscroll
+    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download microsoft/Florence-2-large-ft --include "*.bin" --local-dir /ComfyUI/models/LLM
 fi
