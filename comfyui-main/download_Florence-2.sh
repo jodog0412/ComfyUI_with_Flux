070c177841748254ca23c13ad3fcb22933a657ae
Hyunsung <jodog0412@gmail.com>
2025-02-27 03:07:55 +0900

    ✅ update: update jdg-comfyui 1.1.0

--- comfyui-main/download_Florence-2.sh
+++ comfyui-main/download_Florence-2.sh
@@ -3,12 +3,12 @@
 echo "Downloading Florence 2 models"
 
 dir="/ComfyUI/models/LLM"
-file="Florence-2-large-ft.bin"
-mkdir -p dir
+file="pytorch_model.bin"
+mkdir -p $dir
 
 if [ -f "$dir/$file" ]; then
     echo "$dir/$file already exists."
 else
     echo "Downloading $file"
-    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download microsoft/Florence-2-large-ft --include $file --local-dir $dir
+    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download microsoft/Florence-2-large-ft --local-dir $dir
 fi
--- comfyui-main/download_Janus-Pro.sh
+++ comfyui-main/download_Janus-Pro.sh
@@ -3,12 +3,12 @@
 echo "Downloading Janus-Pro-1B model"
 
 dir="/ComfyUI/models/Janus-Pro/Janus-Pro-1B"
-file="Janus-Pro-1B.bin"
+file="pytorch_model.bin"
 mkdir -p $dir
 
 if [ -f "$dir/$file" ]; then
     echo "$dir/$file already exists."
 else
     echo "Downloading $file"
-    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download deepseek-ai/Janus-Pro-1B --include $file --local-dir $dir
+    HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download deepseek-ai/Janus-Pro-1B --local-dir $dir
 fi
