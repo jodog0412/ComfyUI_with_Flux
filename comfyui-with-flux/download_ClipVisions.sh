96240f04b8c409f9afbb0501df64e1927733d20d
Hyunsung <jodog0412@gmail.com>
2025-02-28 03:27:53 +0900

    ✅ update: update fluxwithcomfyui 3.1.0

--- comfyui-with-flux/download_ClipVisions.sh
+++ comfyui-with-flux/download_ClipVisions.sh
@@ -4,19 +4,16 @@
 download_file() {
     local repository=$1
     local file=$2
-    local dir="/ComfyUI/models/clip_vision"
+    local dir=$3
     
     mkdir -p $dir
     if [ -f "$dir/$file" ]; then
         echo "File $dir/$file already exists, skipping download."
     else
-        HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download $repository --include "*.safetensors" --local-dir $dir
-        if [ -f "$dir/model.safetensors" ]; then
-            mv "$dir/model.safetensors" "$dir/$file" 
-        fi
+        HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download $repository --include $file --local-dir $dir
     fi
 }
 
-download_file "openai/clip-vit-large-patch14" "clip_vit-large.safetensors"
-download_file "google/siglip-so400m-patch14-384" "siglip-so400m.safetensors"
-download_file "Comfy-Org/sigclip_vision_384" "sigclip_vision_patch14_384.safetensors"
+download_file "openai/clip-vit-large-patch14" "model.safetensors" "/ComfyUI/models/clip_vision/clip-vit-large"
+download_file "google/siglip-so400m-patch14-384" "model.safetensors" "/ComfyUI/models/clip_vision/siglip-so400m"
+download_file "Comfy-Org/sigclip_vision_384" "sigclip_vision_patch14_384.safetensors" "/ComfyUI/models/clip_vision"
